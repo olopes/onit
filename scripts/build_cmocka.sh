d81c4b3a677ea081842196cf9a0f9d9730383d31
olopes <psicover.dev@gmail.com>
2019-02-07 00:15:22 +0000

    use curl instead of wget in build_cmocka.sh because stoopid devices

--- scripts/build_cmocka.sh
+++ scripts/build_cmocka.sh
@@ -1,9 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 #require cmocka https://cmocka.org/
 mkdir -p build
 pushd build
-wget https://cmocka.org/files/1.1/cmocka-1.1.3.tar.xz
+# wget https://cmocka.org/files/1.1/cmocka-1.1.3.tar.xz
+curl https://cmocka.org/files/1.1/cmocka-1.1.3.tar.xz --output cmocka-1.1.3.tar.xz
 tar xf cmocka-1.1.3.tar.xz
 cd cmocka-1.1.3
 mkdir build
