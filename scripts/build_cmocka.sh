7f567b1ac8f150726aa95f55f5458c431af2f0ad
olopes <psicover.dev@gmail.com>
2019-02-07 00:02:19 +0000

    Script to build cmocka updated

--- scripts/build_cmocka.sh
+++ scripts/build_cmocka.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
 #require cmocka https://cmocka.org/
-cmake -DCMAKE_INSTALL_PREFIX=`pwd`/../../cmocka -DCMAKE_BUILD_TYPE=Debug ..
+mkdir -p build
+pushd build
+wget https://cmocka.org/files/1.1/cmocka-1.1.3.tar.xz
+tar xf cmocka-1.1.3.tar.xz
+cd cmocka-1.1.3
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=../../../cmocka -DCMAKE_BUILD_TYPE=Debug ..
+make && make install
+cd ../..
+rm -fr cmocka-1.1.3
+rm -fr cmocka-1.1.3.tar.xz
+popd
 
